cf67e7a79bd31beae646cba0d27a724785956804
Samuel Haile <samhaile129@gmail.com>
2022-12-07 21:04:56 -0500

    Converted phonetic alphabet csv>dataframe> dict

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pandas
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+print(df)
+code_dict = {row.letter: row.code for (index,row) in df.iterrows()}
+print(code_dict)
 
 
 
